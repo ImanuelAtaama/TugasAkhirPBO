e1cab2061664c3e3691964e2cc687862e2106f93
ImanuelAtaama <imanuelataama11@gmail.com>
2022-06-25 23:51:58 +0700

    Menambahkan File

---
+++ Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PBO_Imanuel._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container mt-3">
+        <div runat="server" ID="coll" class="d-flex flex-column">
+          
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Tambah.aspx.cs
@@ -0,0 +1,46 @@
+﻿using PBO_Imanuel.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PBO_Imanuel
+{
+    public partial class Tambah : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void insertData(object sender, EventArgs e)
+        {
+            Dictionary<string, dynamic> data = new Dictionary<string, dynamic>();
+            data.Add("model", model.Text);
+            data.Add("jumlah_silinder", silinder.Text);
+            data.Add("harga", harga.Text);
+            data.Add("dimensi", dimensi.Text);
+            data.Add("sistem_penggerak", sistem.Text);
+            data.Add("kapasitas_tangki", kapasitas.Text);
+            data.Add("mesin", mesin.Text);
+            data.Add("daya", daya.Text);
+            data.Add("imgUrl", imageUrl.Text);
+
+            var res = MobilDB.createMobil(data);
+             
+            if (res == "berhasil")
+            {
+                Response.Redirect("~/");
+            }
+            else
+            {
+                result.Text = res;
+            }
+            
+        }
+    }
+
+
+}
